cbc650aae5854efa0eb976173a12d5e46a78dce6
Ahmed <shiekhahmed135@gmail.com>
2021-04-10 20:09:12 +0500

    success

--- src/forms/Payment.tsx
+++ src/forms/Payment.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import Loading from '../components/Loading'
-import checkCreateEntryData from '../errors/check/checkCreateEntryData'
-import createEntry from '../hooks/createEntry'
-import useSite from '../hooks/useSite'
+import parse from "html-react-parser";
+import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import checkCreateEntryData from "../errors/check/checkCreateEntryData";
+import createEntry from "../hooks/createEntry";
+import useSite from "../hooks/useSite";
 
 interface PaymentProps {
-  toSuccess: () => void
-  teamId: number
-  userId: number
-  tournament: TournamentRegister['tournament']
+  toSuccess: () => void;
+  teamId: number;
+  userId: number;
+  tournament: TournamentRegister["tournament"];
 }
 
 const Payment: React.FC<PaymentProps> = ({
@@ -17,9 +18,10 @@ const Payment: React.FC<PaymentProps> = ({
   userId,
   tournament,
 }) => {
-  const [error, setError] = useState('')
-  const [entry, setEntry] = useState<Entry>(null)
-  const site = useSite()
+  const [error, setError] = useState("");
+  const [entry, setEntry] = useState<Entry>(null);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const site = useSite();
   useEffect(() => {
     const fn = async () => {
       if (!entry) {
@@ -27,55 +29,64 @@ const Payment: React.FC<PaymentProps> = ({
           team: teamId,
           tournament: tournament.id,
           user: userId,
-        }
-        let { isValid, message } = checkCreateEntryData(values)
+        };
+        let { isValid, message } = checkCreateEntryData(values);
         if (isValid) {
-          const en = await createEntry(values)
+          const en = await createEntry(values);
           if (en) {
-            setEntry(en)
+            setEntry(en);
+            setHasLoaded(true);
           }
         } else {
-          setError(message)
+          setError(message);
         }
       }
-    }
-    fn()
-  }, [teamId, tournament, userId, entry])
-  if (entry && entry.entry_id && entry.has_paid)
+    };
+    fn();
+  }, [teamId, tournament, userId, entry]);
+  // entry && entry.entry_id && entry.has_paid
+  if (hasLoaded === true)
     return (
       <>
-        <div className='payment-page text-white'>
-          <div className='payment'>
-            <div className='details'>
-              <div className=''>
+        <div className="payment-page text-white">
+          <div className="payment">
+            <div className="details">
+              <div className="">
                 <h3>Details of Transaction</h3>
-                <p>{site.payement_details}</p>
+                <div>{parse(site.payement_details)}</div>
               </div>
             </div>
 
-            <div className='verify'>
-              <div className='row'>
+            <div className="verify">
+              <div className="row">
                 <h1>Entry No</h1>
-                <h2 className='ml-auto'>#{entry.entry_id}</h2>
+                <h2 className="ml-auto">#{entry.entry_id}</h2>
               </div>
               {tournament.hasFee && (
-                <div className='row'>
+                <div className="row">
                   <h1>Total Cost</h1>
                   <h2>{tournament.price}</h2>
                 </div>
               )}
-              <div className='row'>
+              <div className="row">
                 <h1>Payment Status</h1>
-                <h2 className='ml-auto'>
-                  {entry.has_paid ? 'Paid' : 'Unpaid'}
+                <h2 className="ml-auto">
+                  {entry.has_paid ? "Paid" : "Unpaid"}
                 </h2>
               </div>
             </div>
           </div>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => toSuccess()}
+          >
+            Confirm
+          </button>
         </div>
       </>
-    )
-  return <Loading />
-}
+    );
+  return <Loading />;
+};
 
-export default Payment
+export default Payment;
--- src/forms/Success.tsx
+++ src/forms/Success.tsx
@@ -1,31 +1,34 @@
 import React from "react";
+import useEntries from "../hooks/useEntries";
 
 interface SuccessProps {}
 
 const Success: React.FC<SuccessProps> = () => {
+  const entry = useEntries();
+  console.log(entry)
   return (
     <>
       <div className="success-page">
-        <div className="success">
-          <div className="container bill pt-3">
-            <div className="content">
-              <h2 className="payment-heading">
-                <b>Payment Bill</b>
-              </h2>
-              <div className="payment-details">
-                <div className="left-side">
-                  <div className="heading">Bill To</div>
-                  <div className="bill-body">
-                    <p>Team: </p>
-                    <p>Captain: </p>
-                    <p>Email: </p>
-                    <h3 style={{ textAlign: "right" }}>Fees: Rs.500</h3>
-                  </div>
+        <div className="container bill pt-3 text-white">
+          <div className="content">
+            <h2 className="payment-heading">
+              <b>Payment Bill</b>
+            </h2>
+            <div className="payment-details">
+              <div className="left-side">
+                <div className="heading">Bill To</div>
+                <div className="bill-body">
+                  <p>Team: </p>
+                  <p>Captain: </p>
+                  <p>Email: </p>
+                  <h3 style={{ textAlign: "right" }}>Fees: Rs.500</h3>
                 </div>
               </div>
-              <small>Upper details are also sent to you by mail</small>
             </div>
+            <small>Upper details are also sent to you by mail</small>
           </div>
+        </div>
+        <div className="success">
           <h3>
             Your application is submitted successfully{" "}
             <svg
